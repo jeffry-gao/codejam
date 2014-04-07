bab585a00a0e264d821c6006b6cd1320ee29b5a5
Jeffry <jeffry.gao@gmail.com>
2014-04-07 23:26:17 +0900

    solved large

--- 2013-1ca/1ca.cpp
+++ 2013-1ca/1ca.cpp
@@ -48,13 +48,13 @@ int solveSmall(char* name, int N)
 	return s;
 }
 
-int solve(char* name, int N)
+long solve(char* name, int N)
 {
-	cout<<name<<","<<N<<endl;
+	if(debug)cout<<name<<","<<N<<endl;
     char *p1 = name;
     char *p2 = p1;
 	char *p3=NULL, *p4=NULL;
-    int status=0;
+    long s=0;
     while(*p1!=0){
 		while(*p1!=0&&isVow(*p1))p1++;
 		if(debug)cout<<"p1:"<<p1-name<<endl;
@@ -66,32 +66,58 @@ int solve(char* name, int N)
 		while(*p2!=0&&!isVow(*p2))p2++;
 		if(debug)cout<<"p2:"<<p2-name<<endl;
 
+		if(p2-p1<N){
+			if(debug)cout<<"less than "<<N<<endl;
+			p1=p2;
+			continue;
+		}
 		if(!*p2==0){
 			p3=p2;
-			while(*p3!=0&&isVow(*p3))p3++;
-			if(debug)
-				cout<<"p3:"<<p3-name<<endl;
-			if(*p3==0){
+			while(*p3!=0){
+				while(*p3!=0&&isVow(*p3))p3++;
 				if(debug)
-				cout<<"*p3==0"<<endl;
+					cout<<"p3:"<<p3-name<<endl;
+				if(*p3==0){
+					if(debug)
+					cout<<"*p3==0"<<endl;
+				}
+				p4=p3;
+				while(*p4!=0&&!isVow(*p4))
+					p4++;
+				if(debug)
+					cout<<"p4:"<<p4-name<<endl;
+				if(p4-p3>N-1){
+					p4=p3+N-1;	
+					break;
+				}
+				p3=p4;
 			}
-			p4=p3;
-			while(*p4!=0&&!isVow(*p4))
-				p4++;
-			if(debug)
-				cout<<"p4:"<<p4-name<<endl;
-			if(p4-p3>N)
-				p4=p3+N;	
 		}else{
 			p4=p2;
 		}
-		if(debug)cout<<"cal("<<p1-home<<","
-							<<p2-home<<","
-							<<p4-home<<endl;
-		
+		if(debug)cout<<"cal("<<p1-name<<","
+							<<p2-name<<","
+							<<p4-name<<endl;
+		if(debug)cout<<"s:"<<s<<endl;
+		s += (p2-p1-N+1+1)*(p2-p1-N+1)/2;
+		if(debug)cout<<"s:"<<s<<endl;
+		s += (p1-name)*(p4-p1-N+1);
+		if(debug)cout<<"s:"<<s<<endl;
+		s += (p4-p2)*(p2-name-N+1);
+		if(debug)cout<<"s:"<<s<<endl;
+		s -= (p1-name)*(p4-p2);
+		if(debug)cout<<"s:"<<s<<endl;
+		if(*p2==0)
+			break;
+		else{
+			if(*p3==0)
+				break;
+			else
+				p1=p3;
+		}
     }
 
-    return status;
+    return s;
 }
 
 void make()
@@ -104,7 +130,7 @@ void make()
         char name[1000000];
         memset(name,0,sizeof(name));
         cin>>name>>N;
-        int r = solve(name,N);
+        long r = solve(name,N);
         cout<<"Case #"<<c<<": "<<r<<endl;
     }
 }
