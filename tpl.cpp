d4ae838c099deb8d8b4e95f86c2c701294325d9f
Jeffry <jeffry.gao@gmail.com>
2014-04-07 23:35:48 +0900

    simpfied

--- tpl.cpp
+++ tpl.cpp
@@ -8,56 +8,12 @@ using namespace std;
 
 bool debug=false;
 
-void u2(){
-	char s[]="abcd";
-	int   l=strlen(s);
-	
-	vector<string> sl,temp;
-	string tt;
-	tt += (s[0]);
-	sl.push_back(tt);
-    for(int i=1;i<l;i++){
-		cout<<sl.size()<<endl;
-		temp=sl;
-		sl.clear();
-		for(int j=0;j<temp.size();j++){
-			for(int k=0;k<temp[j].size();k++){
-				tt=temp[j];
-				sl.push_back(tt.insert(k,1,s[i]));
-			}
-			sl.push_back(temp[j]+s[i]);
-		}
-	}
-	for(int i=0;i<sl.size();i++)
-		cout<<"["<<i<<"]"<<sl[i]<<endl;
-}
-
-void u1(){
-	char s[]="abcd";
-	int   l=strlen(s);
-    for(int i=0;i<1<<l;i++){
-		cout<<i<<":";
-		for(int j=0;j<l;j++){
-			if(i&(1<<j))
-				cout<<s[j];
-		}
-		cout<<endl;
-	}
-}
-
-int solve(string idt,int N, int* Ns)
+long solveCase(string idt, int N, int* Ns)
 {
-	if(N==0)
-		return 0;
-	else if(N==1){
-		return Ns[0];
-	}else{
-		return Ns[0]+solve(idt,N-1,Ns+1);
-	}
 	return 0;
 }
 
-void make()
+void solve()
 {
 	int T;
 	cin>>T;
@@ -69,7 +25,7 @@ void make()
 		int i=-1;
 		while((++i)<N)
 			cin>>Ns[i];
-		int r = solve("",N,Ns);
+		long r = solveCase("",N,Ns);
 		delete Ns;
 		cout<<"Case #"<<c<<": "<<r<<endl;
 	}
@@ -79,7 +35,6 @@ int main(int argc, char** args)
 {
 	if(argc>1)
 		debug=true;
-//    make();
-	u2();
+    solve();
 }
 
