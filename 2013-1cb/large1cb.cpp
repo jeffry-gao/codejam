80b66dc9f91f819d53d8ee690bba9da99f1db644
jeffry-gao <jeffry.gao@gmail.com>
2014-04-10 14:33:52 +0900

    Update large1cb.cpp
    solved the large

--- 2013-1cb/large1cb.cpp
+++ 2013-1cb/large1cb.cpp
@@ -3,29 +3,42 @@
 
 using namespace std;
 
+#define addup(s) ((s)+1)*(s)/2
+
 string solve(int X, int Y)
 {
-	string path;
-	int Xabs = X<0?-X:X;
-	int Yabs = Y<0?-Y:Y;
-	int s = 1;
-	while(((s+1)*s)/2<Xabs+Yabs)
-		s++;
-	int back = ((s+1)*s/2-Xabs-Yabs)/2;
-	
+    string path;
+    int Xabs = X<0?-X:X;
+    int Yabs = Y<0?-Y:Y;
+    int s = 1;
+    while(addup(s)<Xabs+Yabs||(addup(s)-Xabs-Yabs)%2==1)s++;
+    int b1 = (addup(s)-Xabs-Yabs)/2;
+    int b2 = (b1>s)?b1-s:0;
+    b1 = b1 - b2;
+
+    for(int i=s;i>0;i--) {
+        // i.e. (-2,0) X forward 1, X forward 3, X backward 2
+        if(Y==0)
+            path.insert(0,1, (i==b1||(b2!=0&&b2==i))?(X>0?'W':'E'):(X>0?'E':'W') );
+        else
+            // not all the case can be solved by the method below, so the case Y==0 above is necessary
+            if(i!=b1&&(b2==0||i!=b2)&&Xabs>=i){
+                Xabs -= i;
+                path.insert(0,1,(X>0?'E':'W'));
+            } else
+                path.insert(0,1, (i==b1||(b2!=0&&b2==i))?(Y>0?'S':'N'):(Y>0?'N':'S'));
+    }
+
+    return path;
 }
 
 int main(int argc, char** args)
 {
-	int T;
-	cin>>T;
-	int c=0;
-	while((++c)<=T){
-		int X,Y;
-	 	cin>>X>>Y;
-		string stepX = X>0?"WE":"EW";
-		string stepY = Y>0?"SN":"NS";
-		cout<<"Case #"<<c<<": "
-			<<solve(X,Y)<<endl;
-	}
+    int T,X,Y;
+    cin>>T;
+    int c=0;
+    while((++c)<=T){
+        cin>>X>>Y;
+        cout<<"Case #"<<c<<": "<<solve(X,Y)<<endl;
+    }
 }
