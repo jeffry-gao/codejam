8a0482b7111bc0807c0c58e19b12becd5367e256
Jeffry Gao <jeffry@raspberrypi.(none)>
2014-04-14 19:22:55 +0900

    ...

--- 2014/qd.cpp
+++ 2014/qd.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
-
 void solve(int c)
 {
     int N;
     cin>>N;
-    double Naomi[1000];
-    double Ken[1000];
-    for(int i=0;i<N;i++)
-        cin>>Naomi[i];
-    for(int i=0;i<N;i++)
-        cin>>Ken[i];
+    double Naomi[1000],Ken[1000];
+    for(int i=0;i<N;i++)cin>>Naomi[i];
+    for(int i=0;i<N;i++)cin>>Ken[i];
     sort(Naomi,Naomi+N);
     sort(Ken,Ken+N);
-
     int y=0,z=N;
     for(int i=0,j=0;i<N;i++){
         if(Naomi[i]>Ken[j]){
@@ -23,12 +17,10 @@ void solve(int c)
             j++;
         }
     }
-
     for(int i=0,j=0;j<N;i++,j++){
         while(j<N&&Ken[j]<Naomi[i])j++;
         if(j<N)z--;
     }
-
     cout<<"Case #"<<c<<": "<<y<<" "<<z<<endl;
 }
 
